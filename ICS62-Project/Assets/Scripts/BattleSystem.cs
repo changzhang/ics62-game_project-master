8c06f15c479f58c4c067b10f2652cdc334824ce4
changzhang <albertc7@uci.edu>
2015-05-12 20:02:04 -0700

    testing
    seeing if this works

---
+++ ICS62-Project/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleSystem : MonoBehaviour {
+
+	public int health;
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GetComponent("BattleSystem").enabled = true)
+		{
+			GetComponent("PlayerMovement").enabled = false;
+		}
+		elif (GetComponent ("BattleSystem").enabled = false)
+		{
+			GetComponent("PlayerMovement").enabled = true;
+		}
+	}
+
+	void Battle()
+	{
+		while(health > 0)
+		{
+			Random rnd = new Random();
+			int question = rnd.Next(0,number_of_questions);
+
+
+		}
+	}
+}
---
+++ ICS62-Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * speed * Time.deltaTime);
+		transform.Translate (Vector3.forward * Input.GetAxis ("Vertical") * speed * Time.deltaTime);
+	
+	}
+}
